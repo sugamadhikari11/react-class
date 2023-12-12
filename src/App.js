2dfb1329e786c3e9831f080029c13d2d39148673
Sugam Adhikari <sugam.19217113@gamil.com>
2023-12-12 07:30:46 +0545

    made changes to counter and appp

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
 import './App.css';
-import { useState } from 'react';
+import Counter from './counter';
 
 function App() {
-
-  const [count, setcount] = useState(0)
-  const [initialCount, setInitialCount] = useState(0);
-
-  const addButton = function(){
-    setcount(count + 1)
-  }
-  const subButton = () => {
-    if (count > 0){
-      setcount(count - 1)
-    }
-  }
-  const reset = ()=>{
-    setcount(initialCount);
-  }
-    
   return (
     <div>
-   <div className='container'>
-    <p>
-    <h1>Hello world! I am counter!</h1>
-    </p>
-    <button id="sub" onClick={subButton}>-</button>
-    <span id="counter">{count}</span>
-    <button id="add" onClick={addButton}>+</button>
-    <button onClick={reset}>Reset</button>
-   </div>
-   {count !== 0 ?<div className='container'>
-    {count} is {count%2 ===0 ? "Even": "Odd"}
-   </div> : null}
-    
-   </div>
+      <Counter />
+    </div>
   );
-
+  
 }
 
 export default App;
--- src/counter.js
+++ src/counter.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import { useState } from 'react';
 
-export default function counter() {
-  return (
-    <div>counter</div>
+function Counter() {
+
+    const [count, setcount] = useState(0)
+    const [initialCount, setInitialCount] = useState(0);
+  
+    const addButton = function(){
+      setcount(count + 1)
+    }
+    const subButton = () => {
+      if (count > 0){
+        setcount(count - 1)
+      }
+    }
+    const reset = ()=>{
+      setcount(initialCount);
+    }
+      
+    return (
+      <div>
+     <div className='container'>
+      <p>
+      <h1>Hello world! I am counter!</h1>
+      </p>
+      <button id="sub" onClick={subButton}>-</button>
+      <span id="counter">{count}</span>
+      <button id="add" onClick={addButton}>+</button>
+      <button onClick={reset}>Reset</button>
+     </div>
+     {count !== 0 ?<div className='container'>
+      {count} is {count%2 ===0 ? "Even": "Odd"}
+     </div> : null}
+      
+     </div>
+    );
+  
     
-  )
 }
+
+export default Counter;
