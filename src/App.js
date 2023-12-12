e97e11739561cef61022f47524b49147ea3b314a
Sugam Adhikari <sugam.19217113@gamil.com>
2023-12-12 07:17:25 +0545

    frontend updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [count, setcount] = useState(0)
+  const [initialCount, setInitialCount] = useState(0);
+
+  const addButton = function(){
+    setcount(count + 1)
+  }
+  const subButton = () => {
+    if (count > 0){
+      setcount(count - 1)
+    }
+  }
+  const reset = ()=>{
+    setcount(initialCount);
+  }
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+   <div className='container'>
+    <p>
+    <h1>Hello world! I am counter!</h1>
+    </p>
+    <button id="sub" onClick={subButton}>-</button>
+    <span id="counter">{count}</span>
+    <button id="add" onClick={addButton}>+</button>
+    <button onClick={reset}>Reset</button>
+   </div>
+   {count !== 0 ?<div className='container'>
+    {count} is {count%2 ===0 ? "Even": "Odd"}
+   </div> : null}
+    
+   </div>
   );
+
 }
 
 export default App;
